4aa55c679e05d70d8713c6b92b4733f90fc630b3
formation <louvet.raphael@gmail.com>
2022-02-14 15:32:55 +0100

    strategie

--- src/main/java/com/api/Wallet/entity/Payment.java
+++ src/main/java/com/api/Wallet/entity/Payment.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -16,7 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class Payment {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private double amount;
 	private LocalDateTime date;
